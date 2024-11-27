95ab0f07f5b4c72f6e34463fb4ed9535c82da059
Kamil Zachara <kamil.zachara98@gmail.com>
2024-11-27 17:22:09 +0100

    Initial Commit

---
+++ QuickEvent/QuickEventApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickEventApp.swift
+//  QuickEvent
+//
+//  Created by Kamil Zachara on 27/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct QuickEventApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
